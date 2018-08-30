212e3d2670d0506fc3ab0a8a1931d6fb701191a3
Jay Marcyes <jay@marcyes.com>
2018-08-30 17:08:24 -0600

    adds more py3 versions

--- setup.py
+++ setup.py
@@ -37,6 +37,8 @@ setup(
         'Topic :: Software Development :: Testing',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
     ],
     test_suite = "{}_test".format(name),
 )
